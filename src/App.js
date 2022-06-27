b7aecfac470faa4145ce3e6ba74762864dcd7853
luull <m.fadlullah14@gmail.com>
2022-06-27 13:53:07 +0700

    init file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+import Topbar from "./templates/Topbar";
+import Footer from "./templates/Footer";
+
+import LandingPage from "./pages/LandingPage";
+import "./App.css";
+import "./custom.scss";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Topbar />
+      <div>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/templates/Topbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+export default function Topbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <span className="navbar-toggler-icon"></span>
+        </div>
+      </nav>
+    </div>
+  );
+}
