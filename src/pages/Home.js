ba9d5c88a1249945c3882670097e1db609473b36
luull <m.fadlullah14@gmail.com>
2022-06-30 19:40:12 +0700

    patch#9:checkpoint slider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,13 +15,6 @@ export default function Home() {
   }, []);
   return (
     <div>
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-        crossorigin="anonymous"
-        referrerpolicy="no-referrer"
-      />
       <nav
         className="navbar navbar-light"
         style={{
