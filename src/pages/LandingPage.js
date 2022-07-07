f122b10c03897cb6549df7d4774c5ac40694838c
luull <m.fadlullah14@gmail.com>
2022-07-07 23:55:58 +0700

    responsive finish

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -19,6 +19,8 @@ export default function LandingPage() {
         style={{
           margin: 0,
           padding: 0,
+          minHeight: "700px",
+          overflowY: "scroll",
           overflowX: "hidden",
         }}
       >
@@ -152,53 +154,56 @@ export default function LandingPage() {
             </div>
           </div>
         </div> */}
+
         <div
-          className="bwh row justify-content-center fixed-bottom"
+          className="row justify-content-center"
           style={{
             position: "relative",
             marginTop: "-45px",
-            overflow: "hidden",
+            overflowX: "hidden",
             // marginBottom: "100px",
-            paddingTop: "10px",
-            paddingBottom: "15px",
-            background: "#1616ab",
+            // background: "#1616ab",
           }}
         >
-          <div className="col-4 col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
-            <div className="text-center d-grid">
-              <a
-                onClick={() => navigate("/register")}
-                className="btn btn-warning btn-block"
-                style={{
-                  paddingTop: "7px",
-                  paddingBottom: "7px",
-                  background: "#f9af02",
-                  textTransform: "uppercase",
-                  fontSize: "13px",
-                  fontWeight: "800",
-                }}
-              >
-                Daftar
-              </a>
-            </div>
-          </div>
+          <div className="bwh">
+            <div className="row justify-content-center">
+              <div className="col-4 col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                <div className="text-center d-grid">
+                  <a
+                    onClick={() => navigate("/register")}
+                    className="btn btn-warning btn-block"
+                    style={{
+                      paddingTop: "7px",
+                      paddingBottom: "7px",
+                      background: "#f9af02",
+                      textTransform: "uppercase",
+                      fontSize: "13px",
+                      fontWeight: "800",
+                    }}
+                  >
+                    Daftar
+                  </a>
+                </div>
+              </div>
 
-          <div className="col-4 col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
-            <div className="text-center d-grid">
-              <a
-                onClick={() => navigate("/login")}
-                className="btn btn-warning btn-block"
-                style={{
-                  paddingTop: "7px",
-                  paddingBottom: "7px",
-                  background: "#f9af02",
-                  textTransform: "uppercase",
-                  fontSize: "13px",
-                  fontWeight: "800",
-                }}
-              >
-                Masuk
-              </a>
+              <div className="col-4 col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                <div className="text-center d-grid">
+                  <a
+                    onClick={() => navigate("/login")}
+                    className="btn btn-warning btn-block"
+                    style={{
+                      paddingTop: "7px",
+                      paddingBottom: "7px",
+                      background: "#f9af02",
+                      textTransform: "uppercase",
+                      fontSize: "13px",
+                      fontWeight: "800",
+                    }}
+                  >
+                    Masuk
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
