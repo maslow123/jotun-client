db00c5c0d79c4e1efe3739b65c8026ceef709fc7
luull <m.fadlullah14@gmail.com>
2022-07-14 16:41:03 +0700

    dev##83:finish

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Background from "./../../BG1.svg";
+import Background from "./../BG1.svg";
 
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
